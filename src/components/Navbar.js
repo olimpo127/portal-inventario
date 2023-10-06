0e0647faf68238d6411f2475e734f52b23725662
Felipe Ojeda <fojedai47@gmail.com>
2023-10-06 18:01:39 -0300

    navbar display fixes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,21 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Navbar.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 function Navbar() {
   return (
-    <div className='navbar'>
-      <Link to='/' className='navbar-button navbar-home'>
+    <div className="navbar">
+      <Link to="/" className="navbar-home">
         Home
       </Link>
-      <Link to="/signup" className='navbar-button navbar-signup'>
-        Sign Up
-      </Link>
-      <Link to="/login" className='navbar-button navbar-login'>
-        Login
-      </Link>
+      <div className="bottomButtons">
+        <Link to="/signup" className="navbar-button navbar-signup">
+          Sign Up
+        </Link>
+        <Link to="/login" className="navbar-button navbar-login">
+          Login
+        </Link>
+      </div>
     </div>
   );
 }
