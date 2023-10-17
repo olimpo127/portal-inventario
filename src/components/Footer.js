1f3f2c15c9badb28f772f6e543cbd268f1ee4e92
Felipe Ojeda <fojedai47@gmail.com>
2023-10-17 17:01:32 -0300

    minor footer fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import "./Footer.css";
 function Footer() {
   return (
     <div className='footer'>
-      This is my footer
+      <a href="https://github.com/olimpo127" target="_blank" className='footer-message'>Created by FELIPE OJEDA, Go visit my GitHub</a>
     </div>
   )
 }
