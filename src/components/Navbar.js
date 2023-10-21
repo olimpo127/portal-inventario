a41375458d5f88023309b997cd1bb12c13733d39
Felipe Ojeda <fojedai47@gmail.com>
2023-10-20 21:36:04 -0300

    login/logout fix when user is in or out

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,16 +3,18 @@ import { Link } from "react-router-dom";
 import { useNavigate } from 'react-router-dom';
 import "./Navbar.css";
 
-
-
 function Navbar() {
   const navigate = useNavigate();
+
+  const jwtToken = localStorage.getItem('jwtToken'); // Check if a JWT token exists
+
   const handleLogout = () => {
     // Clear the JWT token from local storage
     localStorage.removeItem('jwtToken');
     // Redirect to the login page
     navigate('/login');
   };
+
   return (
     <div className="navbar">
       <Link to="/" className="navbar-home">
@@ -22,13 +24,21 @@ function Navbar() {
         Tasks
       </Link>
       <div className="bottomButtons">
-        <Link to="/signup" className="navbar-button navbar-signup">
-          Sign Up
-        </Link>
-        <Link to="/login" className="navbar-button navbar-login">
-          Login
-        </Link>
-        <button onClick={handleLogout} className="logout logout-button">Logout</button>
+        {/* Conditional rendering of buttons */}
+        {!jwtToken ? (
+          <React.Fragment>
+            <Link to="/signup" className="navbar-button navbar-signup">
+              Sign Up
+            </Link>
+            <Link to="/login" className="navbar-button navbar-login">
+              Login
+            </Link>
+          </React.Fragment>
+        ) : (
+          <button onClick={handleLogout} className="logout logout-button">
+            Logout
+          </button>
+        )}
       </div>
     </div>
   );
