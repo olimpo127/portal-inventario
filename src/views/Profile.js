2af17a674f044e92181aeee23d6c7465ec01ed24
Felipe Ojeda <fojedai47@gmail.com>
2023-11-16 09:16:32 -0300

    update post first steps

--- src/views/Profile.js
+++ src/views/Profile.js
@@ -4,6 +4,7 @@ import "./Profile.css";
 function Profile() {
   const [user, setUser] = useState({});
   const [loading, setLoading] = useState(true);
+  const [updatePost, setUpdatePost] = useState(false);
 
   useEffect(() => {
     const fetchUserProfile = async () => {
@@ -11,7 +12,7 @@ function Profile() {
         const response = await fetch("http://localhost:5000/profile/", {
           method: "GET",
           headers: {
-            "Authorization": `Bearer ${localStorage.getItem("jwtToken")}`,
+            Authorization: `Bearer ${localStorage.getItem("jwtToken")}`,
           },
         });
         if (response.status === 200) {
@@ -32,12 +33,12 @@ function Profile() {
 
   const handleDelete = async (id) => {
     console.log("Delete button clicked with id:", id);
-  
+
     try {
       const response = await fetch(`http://localhost:5000/posts/${id}`, {
         method: "DELETE",
       });
-  
+
       if (response.ok) {
         console.log("Post deleted");
       } else {
@@ -47,7 +48,10 @@ function Profile() {
       console.error("Error:", error);
     }
   };
-  
+
+  const handleUpdatePost = () => {
+    setUpdatePost(true);
+  };
 
   return (
     <div className="profile">
@@ -60,39 +64,76 @@ function Profile() {
           <p>Lastname: {user.lastname}</p>
           <p>Username: {user.username}</p>
           <p>Email: {user.email}</p>
-
+  
           <h2>My Posts</h2>
-          <div className="posts"> 
-          {user.posts && user.posts.length > 0 ? (
-            <ul>
-              {user.posts.map((post) => (
-                <li key={post.id} className="singlePost">
-                  <strong>Service:</strong> {post.service}
-                  <br />
-                  <strong>Price:</strong> {post.price}
-                  <br />
-                  <strong>Location:</strong> {post.location}
-                  <br />
-                  <strong>Option:</strong> {post.option}
-                  <br />
-                  <button
-                className="deleteButton"
-                onClick={() => handleDelete(post.id)}
-              >
-                Delete Post
-              </button>
-                </li>
-              ))}
-            </ul>
-          ) : (
-            <p>No posts found.</p>
-          )}
+          <div className="posts">
+            {user.posts && user.posts.length > 0 ? (
+              <ul>
+                {user.posts.map((post) => (
+                  <li key={post.id} className="singlePost">
+                    <strong>Service:</strong> {post.service}
+                    <br />
+                    <strong>Price:</strong> {post.price}
+                    <br />
+                    <strong>Location:</strong> {post.location}
+                    <br />
+                    <strong>Option:</strong> {post.option}
+                    <br />
+                    <button
+                      className="deleteButton"
+                      onClick={() => handleDelete(post.id)}
+                    >
+                      Delete Post
+                    </button>
+                    <button
+                      className="updateButton"
+                      onClick={() => handleUpdatePost(post.id)}
+                    >
+                      Update Post
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p>No posts found.</p>
+            )}
           </div>
-          
+  
+          {/* Conditional Form Display */}
+          {updatePost && (
+            <div className="updateForm">
+              <form /*onSubmit={handleUpdate}*/ className="signUpForm">
+          <input
+            type="text"
+            name="service"
+            // value={updateUser.name}
+            // onChange={handleUpdateInputChange}
+            placeholder="Service"
+          />
+          <input
+            type="text"
+            name="price"
+            //value={updateUser.lastname}
+            //onChange={handleUpdateInputChange}
+            placeholder="Price"
+          />
+          <input
+            type="text"
+            name="location  "
+            //value={updateUser.username}
+            //onChange={handleUpdateInputChange}
+            placeholder="Location"
+          />
+          <button type="submit" className="createUserButton">Update Post</button>
+          <button type="button" onClick={() => setUpdatePost(false)} className="createUserButton">Cancel</button>
+        </form>
+            </div>
+          )}
         </div>
       )}
     </div>
   );
+  
 }
 
 export default Profile;
